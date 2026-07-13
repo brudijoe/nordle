09957543315e40c3855b9a936a371b15172f0d9c
brudijoe <doningo@ymail.com>
2026-07-13 12:34:10 +0200

    feat: add basic login from expo router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,33 @@
-import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Stack } from 'expo-router';
+import { SessionProvider, useSession } from '@/ctx';
+import { SplashScreenController } from '@/splash';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <SplashScreenController />
+        <RootNavigator />
+      </SessionProvider>
+    </QueryClientProvider>
+  );
+}
+
+function RootNavigator() {
+  const { session } = useSession();
+
+  return (
+    <Stack>
+      <Stack.Protected guard={!!session}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
+    </Stack>
+  );
 }
