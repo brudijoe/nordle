600b524f02157aa7222a7be456daee2d3ebed475
brudijoe <doningo@ymail.com>
2026-08-27 10:30:51 +0200

    feat: add react-native-paper

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PaperProvider } from 'react-native-paper';
 
 export const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <PaperProvider>
+        <Stack />
+      </PaperProvider>
     </QueryClientProvider>
   );
 }
